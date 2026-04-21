e8f16c6721c98a4d0c0a89c6f2aeb83387b449e5
MangoFanFanw <mangofanfanw@icloud.com>
2026-04-21 22:23:21 +0800

    测试 x3
    在次测试，一定要把图片截出来！

--- router/enhance/screenshot.py
+++ router/enhance/screenshot.py
@@ -25,7 +25,9 @@ class ScreenShot(PlayContextManager):
     async def shot(self, save_path: str) -> None:
         await self.page.mouse.move(0, 0)
         try:
-            await self.page.wait_for_load_state("networkidle", timeout=1000)
+            await self.page.wait_for_load_state(timeout=10000)
+        except Exception:
+            pass
         finally:
             await self.page.screenshot(path=save_path)
         logger.debug(f"截图 | 截图已经保存在 {save_path=}")
