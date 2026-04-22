df17a73c3130956703390e235bc77fd9864f2b7b
MangoFanFanw <mangofanfanw@icloud.com>
2026-04-22 23:31:45 +0800

    让顶部导航栏真正可用
    添加顶部导航栏内的链接

--- webui/src/coms/HeaderNav.vue
+++ webui/src/coms/HeaderNav.vue
@@ -5,9 +5,28 @@
     <div id="nav-title">
       <n-h3 class="no-margin-bottom">NJUPT Suan API</n-h3>
     </div>
-    <n-button secondary type="primary">概览</n-button>
-    <n-button secondary type="info">GitHub</n-button>
-    <n-button secondary type="error">芒果</n-button>
+    <a href="https://suan.mangofanfan.cn/" target="_blank">
+      <n-button secondary type="primary">概览</n-button>
+    </a>
+    <n-popover trigger="hover">
+      <template #trigger>
+        <n-button secondary type="info">源码</n-button>
+      </template>
+      <n-p class="no-margin-bottom"
+        >NJUPT Suan API 的代码开源在以下两个地方，后面的那个可能更快？</n-p
+      >
+      <n-flex justify="center">
+        <a href="https://github.com/mangofanfan/NJUPT-Suan-API" target="_blank">
+          <n-button secondary type="info">GitHub</n-button>
+        </a>
+        <a href="https://gitea.mangofanfan.cn/SuanDev/NJUPT-Suan-API" target="_blank">
+          <n-button secondary type="success">Mango Gitea</n-button>
+        </a>
+      </n-flex>
+    </n-popover>
+    <a href="https://mangofanfan.cn/" target="_blank">
+      <n-button secondary type="error">芒果</n-button>
+    </a>
   </div>
 </template>
 
